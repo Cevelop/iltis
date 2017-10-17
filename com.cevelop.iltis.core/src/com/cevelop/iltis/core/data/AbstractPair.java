722398d9cdf65a854b0559c426fd517090d29d8c
Stautob <stautob@stautob.ch>
2017-10-17 13:53:28 +0200

    Moved from github.com

---
+++ com.cevelop.iltis.core/src/com/cevelop/iltis/core/data/AbstractPair.java
@@ -0,0 +1,67 @@
+package com.cevelop.iltis.core.data;
+
+
+/**
+ * A templated abstract implementation of a Pair
+ *
+ * @author tstauber
+ *
+ * @param <T1>
+ *            Template-type of first element
+ * @param <T2>
+ *            Template-type of second element
+ */
+public abstract class AbstractPair<T1, T2> {
+	protected T1 first;
+	protected T2 second;
+
+	public AbstractPair(final T1 first, final T2 second) {
+		this.first = first;
+		this.second = second;
+	}
+
+	@Override
+	public String toString() {
+		return "First: " + first + "\nSecond: " + second;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((first == null) ? 0 : first.hashCode());
+		result = prime * result + ((second == null) ? 0 : second.hashCode());
+		return result;
+	}
+
+	@Override
+	@SuppressWarnings("rawtypes")
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final AbstractPair other = (AbstractPair) obj;
+		if (first == null) {
+			if (other.first != null) {
+				return false;
+			}
+		} else if (!first.equals(other.first)) {
+			return false;
+		}
+		if (second == null) {
+			if (other.second != null) {
+				return false;
+			}
+		} else if (!second.equals(other.second)) {
+			return false;
+		}
+		return true;
+	}
+
+}
---
+++ com.cevelop.iltis.core/src/com/cevelop/iltis/core/preferences/InfrastructureMessages.java
@@ -0,0 +1,16 @@
+package com.cevelop.iltis.core.preferences;
+
+import org.eclipse.osgi.util.NLS;
+
+public class InfrastructureMessages extends NLS {
+  private static final String BUNDLE_NAME = "com.cevelop.iltis.core.preferences.infrastructureMessages"; //$NON-NLS-1$
+  public static String FieldEditorPropertyAndPreferencePage_checkbox_text;
+  public static String FieldEditorPropertyAndPreferencePage_link_text;
+  static {
+    // initialize resource bundle
+    NLS.initializeMessages(BUNDLE_NAME, InfrastructureMessages.class);
+  }
+
+  private InfrastructureMessages() {
+  }
+}
