d7f88b3c163686af43190ee63be2bea9d8ccc91f
Stautob <tobias.stauber@hsr.ch>
2018-01-31 17:39:36 +0100

    Documented and improved CFileUtil

--- ch.hsr.ifs.iltis.cpp/src/ch/hsr/ifs/iltis/cpp/resources/CFileUtil.java
+++ ch.hsr.ifs.iltis.cpp/src/ch/hsr/ifs/iltis/cpp/resources/CFileUtil.java
@@ -9,15 +9,31 @@ import ch.hsr.ifs.iltis.core.resources.FileUtil;
 import ch.hsr.ifs.iltis.core.resources.ProjectUtil;
 
 
-public abstract class CFileUtil extends FileUtil {
+/**
+ * A utility class which offers static methods which use cdt.
+ * Should be used in combination with {@link FileUtil}.
+ * 
+ * @author tstauber
+ */
+public abstract class CFileUtil {
 
-   //DOC missing
+   /**
+    * Used to get the file in which node was defined
+    * 
+    * @return the IFile in which node was defined, or null, if the node was not created from a file.
+    */
    public static IFile getFile(final IASTNode node) {
-      return ProjectUtil.getWorkspaceRoot().getFileForLocation(new Path(node.getFileLocation().getFileName()));
+      IASTFileLocation location = getNodeFileLocation(node);
+      return location == null ? null : ProjectUtil.getWorkspaceRoot().getFileForLocation(new Path(location.getFileName()));
    }
 
-   //DOC missing
+   /**
+    * Used to get the {@code IASTFileLocation} from a {@code IASTNode}
+    * 
+    * @return the IASTFileLocation if node was created from a file, else null
+    */
    public static IASTFileLocation getNodeFileLocation(final IASTNode node) {
+      if (node == null) return null;
       return node.getFileLocation() != null ? node.getFileLocation() : getNodeFileLocation(node.getParent());
    }
 
