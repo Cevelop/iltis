dccf7296e4df50bc791aa05696a1c18472f6842a
Stautob <tobias.stauber@hsr.ch>
2018-02-02 17:08:45 +0100

    Moved normalizeCPP to cdttesting ASTComparison

--- ch.hsr.ifs.iltis.core/src/ch/hsr/ifs/iltis/core/resources/StringUtil.java
+++ ch.hsr.ifs.iltis.core/src/ch/hsr/ifs/iltis/core/resources/StringUtil.java
@@ -8,28 +8,6 @@ public abstract class StringUtil {
 
    public static class CodeString {
 
-      /**
-       * Normalizes the passed {@link String} by removing all testeditor-comments,
-       * removing leading/trailing whitespace and line-breaks, replacing all
-       * remaining line-breaks by ↵ and reducing all groups of whitespace to a
-       * single space.
-       *
-       * @author tstauber
-       *
-       * @param in
-       *        The {@link String} that should be normalized.
-       *
-       * @return A normalized copy of the parameter in.
-       **/
-      public static String normalize(final String in) {
-         //@formatter:off
-         return in.replaceAll("/\\*.*\\*/", "")								//Remove all test-editor-comments
-                  .replaceAll("(^((\\r?\\n)|\\s)*|((\\r?\\n)|\\s)*$)", "")	//Remove all leading and trailing linebreaks/whitespace
-                  .replaceAll("\\s*(\\r?\\n)+\\s*", "↵")						//Replace all linebreaks with linebreak-symbol
-                  .replaceAll("\\s+", " ");									//Reduce all groups of whitespace to a single space
-         //@formatter:on
-      }
-
       /* HTML */
 
       private static final Map<String, String> LOOKUP_TABLE = new LinkedHashMap<>();
