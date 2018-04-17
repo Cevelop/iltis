58d932e0cfa76f5b695d3d6b351ba9cb07c4ffb6
Stautob <tobias.stauber@hsr.ch>
2018-04-17 16:49:19 +0200

    Added Extended Example Project Wizard

---
+++ ch.hsr.ifs.iltis.core/src/ch/hsr/ifs/iltis/core/ui/examples/IExampleProjectConfigurator.java
@@ -0,0 +1,10 @@
+package ch.hsr.ifs.iltis.core.ui.examples;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public interface IExampleProjectConfigurator {
+
+   public void configureProject(IProject project, IProgressMonitor monitor);
+
+}
