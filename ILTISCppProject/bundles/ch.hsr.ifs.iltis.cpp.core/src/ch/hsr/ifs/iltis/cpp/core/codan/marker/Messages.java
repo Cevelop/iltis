841e22b341383f944263abf9d25e3a08d327195a
Stautob <tobias.stauber@hsr.ch>
2018-05-09 16:26:53 +0200

    Fixed error in nls

--- ILTISCppProject/bundles/ch.hsr.ifs.iltis.cpp.core/src/ch/hsr/ifs/iltis/cpp/core/codan/marker/Messages.java
+++ ILTISCppProject/bundles/ch.hsr.ifs.iltis.cpp.core/src/ch/hsr/ifs/iltis/cpp/core/codan/marker/Messages.java
@@ -5,7 +5,7 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 
-   private static final String BUNDLE_NAME = "com.cevelop.intwidthfixator.quickfixes.messages"; //$NON-NLS-1$
+   private static final String BUNDLE_NAME = "ch.hsr.ifs.iltis.cpp.core.codan.marker"; //$NON-NLS-1$
    public static String        PMRG_CannotCompile;
    public static String        PMRG_Invalid;
    public static String        PMRG_NotDefined;
