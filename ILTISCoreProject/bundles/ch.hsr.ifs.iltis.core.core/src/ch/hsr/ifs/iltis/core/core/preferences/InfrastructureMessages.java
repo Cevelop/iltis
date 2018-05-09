e1296ce58c0675624e7f4ba814feab31e79aa70f
Stautob <tobias.stauber@hsr.ch>
2018-05-09 16:27:17 +0200

    BugFix NLS Messages resolving

--- ILTISCoreProject/bundles/ch.hsr.ifs.iltis.core.core/src/ch/hsr/ifs/iltis/core/core/preferences/InfrastructureMessages.java
+++ ILTISCoreProject/bundles/ch.hsr.ifs.iltis.core.core/src/ch/hsr/ifs/iltis/core/core/preferences/InfrastructureMessages.java
@@ -5,7 +5,7 @@ import org.eclipse.osgi.util.NLS;
 
 public class InfrastructureMessages extends NLS {
 
-   private static final String BUNDLE_NAME = "ch.hsr.ifs.iltis.core.preferences.infrastructureMessages"; //$NON-NLS-1$
+   private static final String BUNDLE_NAME = "ch.hsr.ifs.iltis.core.core.preferences.infrastructureMessages"; //$NON-NLS-1$
    public static String        FieldEditorPropertyAndPreferencePage_checkbox_text;
    public static String        FieldEditorPropertyAndPreferencePage_preference_link_text;
    public static String        FieldEditorPropertyAndPreferencePage_property_link_text;
