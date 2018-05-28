4186476288df8a2217d7134db1ddb3a20b64e44a
Felix Morgner <fmorgner@hsr.ch>
2018-05-28 16:22:20 +0200

    testing: port 'Data' tests to Kotlin

---
+++ ILTISCoreProject/tests/ch.hsr.ifs.iltis.core.core.tests/src/ch/hsr/ifs/iltis/core/core/tests/data/WrapperTest.kt
@@ -0,0 +1,28 @@
+package ch.hsr.ifs.iltis.core.core.tests.data;
+
+import org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import ch.hsr.ifs.iltis.core.core.data.Wrapper;
+
+
+class WrapperTest {
+
+   @Test
+   fun lambdaEscape() {
+      val w = Wrapper("")
+
+      val strings = arrayOf("foo", "bar", "baz", "foobar", "foobaz")
+      Arrays.stream(strings).forEach{
+         if (it.contains("foo")) {
+            w.wrapped += it;
+         }
+      }
+
+      assertEquals("foofoobarfoobaz", w.wrapped);
+   }
+
+}
