cc338739bee9ff8e46358b49795f2abdcd4c4c58
Stautob <tobias.stauber@hsr.ch>
2018-09-20 15:47:40 +0200

    collections: cleanup

--- ILTISCoreProject/bundles/ch.hsr.ifs.iltis.core.core/src/ch/hsr/ifs/iltis/core/core/collections/SoftReferenceCache.java
+++ ILTISCoreProject/bundles/ch.hsr.ifs.iltis.core.core/src/ch/hsr/ifs/iltis/core/core/collections/SoftReferenceCache.java
@@ -5,7 +5,6 @@ import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
 import java.lang.ref.WeakReference;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
