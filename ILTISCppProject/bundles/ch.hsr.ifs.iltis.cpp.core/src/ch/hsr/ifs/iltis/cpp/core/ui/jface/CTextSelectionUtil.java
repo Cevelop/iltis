8d305745c7d31d7cf61844b0f91ac037a4dc853c
Stautob <tobias.stauber@hsr.ch>
2018-09-20 15:24:43 +0200

    ui: added utility for TextSelections using cdt

---
+++ ILTISCppProject/bundles/ch.hsr.ifs.iltis.cpp.core/src/ch/hsr/ifs/iltis/cpp/core/ui/jface/CTextSelectionUtil.java
@@ -0,0 +1,27 @@
+package ch.hsr.ifs.iltis.cpp.core.ui.jface;
+
+import org.eclipse.cdt.codan.core.model.IProblemLocation;
+import org.eclipse.cdt.codan.internal.core.model.CodanProblemMarker;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.text.TextSelection;
+
+import ch.hsr.ifs.iltis.core.core.ui.jface.TextSelectionUtil;
+
+
+public class CTextSelectionUtil extends TextSelectionUtil {
+
+   /**
+    * Extracts the cdt IProblemLocation from the marker and tests, if it is contained in the selection.
+    * 
+    * @param outer
+    *        The selection
+    * @param marker
+    *        The marker
+    * @return {@code true} iff the marker is contained in the selection
+    */
+   public static boolean contains(ITextSelection outer, IMarker marker) {
+      IProblemLocation location = CodanProblemMarker.getLocation(marker);
+      return contains(outer, new TextSelection(location.getStartingChar(), location.getEndingChar() - location.getStartingChar()));
+   }
+}
