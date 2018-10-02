40be0b0e99a1d18e970fea905260c359bf397130
Stautob <tobias.stauber@hsr.ch>
2018-10-02 17:49:18 +0200

    format: formatted with new SLOTH formatter

--- ILTISTestingCoreProject/bundles/ch.hsr.ifs.iltis.testing.core.core/src/ch/hsr/ifs/iltis/testing/core/core/validators/ILTISWrapper.java
+++ ILTISTestingCoreProject/bundles/ch.hsr.ifs.iltis.testing.core.core/src/ch/hsr/ifs/iltis/testing/core/core/validators/ILTISWrapper.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2011 Institute for Software, HSR Hochschule fuer Technik
  * Rapperswil, University of applied sciences and others
  * All rights reserved.
- * 
+ *
  * Contributors:
  * Institute for Software - initial API and implementation
  ******************************************************************************/
@@ -20,6 +20,6 @@ import java.lang.annotation.Target;
 @Inherited
 public @interface ILTISWrapper {
 
-   Class<?> value();
+    Class<?> value();
 
 }
--- ILTISTestingCoreProject/bundles/ch.hsr.ifs.iltis.testing.core.core/src/ch/hsr/ifs/iltis/testing/core/core/validators/WrapperValidationTest.java
+++ ILTISTestingCoreProject/bundles/ch.hsr.ifs.iltis.testing.core.core/src/ch/hsr/ifs/iltis/testing/core/core/validators/WrapperValidationTest.java
@@ -15,94 +15,97 @@ import java.util.stream.Stream;
 
 /**
  * A validator for wrapped classes.
- * 
+ *
  * @author tstauber
  *
  */
 public class WrapperValidationTest {
 
-   /**
-    * Validates that every public method from the wrapped class was wrapped in the wrapper.
-    * 
-    * @param wrapingClass
-    *        The wrapper-class annotated with {@link ILTISWrapper}
-    */
-   public <T> void validate(Class<T> wrapingClass) {
-      List<String> missingMethods = new ArrayList<>();
-      ILTISWrapper wraper = wrapingClass.getAnnotation(ILTISWrapper.class);
-      if (wraper == null) throw new IllegalArgumentException("Class " + wrapingClass.getName() + " is not annotated with an ILTISWrapper!");
-      Class<?> wrapedClass = wraper.value();
+    /**
+     * Validates that every public method from the wrapped class was wrapped in the wrapper.
+     *
+     * @param wrapingClass
+     * The wrapper-class annotated with {@link ILTISWrapper}
+     */
+    public <T> void validate(Class<T> wrapingClass) {
+        List<String> missingMethods = new ArrayList<>();
+        ILTISWrapper wraper = wrapingClass.getAnnotation(ILTISWrapper.class);
+        if (wraper == null) throw new IllegalArgumentException("Class " + wrapingClass.getName() + " is not annotated with an ILTISWrapper!");
+        Class<?> wrapedClass = wraper.value();
 
-      List<Method> wrapeMethods = Arrays.asList(wrapedClass.getMethods());
-      List<Method> wraperMethods;
-      if (wrapedClass.isAssignableFrom(wrapingClass)) {
-         wraperMethods = Stream.of(wrapingClass.getDeclaredMethods()).filter(m -> Modifier.isPublic(m.getModifiers())).collect(Collectors.toList());
-      } else {
-         wraperMethods = Arrays.asList(wrapingClass.getMethods());
-      }
+        List<Method> wrapeMethods = Arrays.asList(wrapedClass.getMethods());
+        List<Method> wraperMethods;
+        if (wrapedClass.isAssignableFrom(wrapingClass)) {
+            wraperMethods = Stream.of(wrapingClass.getDeclaredMethods()).filter(m -> Modifier.isPublic(m.getModifiers())).collect(Collectors
+                    .toList());
+        } else {
+            wraperMethods = Arrays.asList(wrapingClass.getMethods());
+        }
 
-      for (Method wrapeeMethod : wrapeMethods) {
-         boolean foundMethod = !wrapeeMethod.getDeclaringClass().getPackage().getName().contains(".internal.");
-         if (!foundMethod) {
-            for (Method wraperMethod : wraperMethods) {
-               foundMethod |= compareMethods(wrapeeMethod, wraperMethod);
+        for (Method wrapeeMethod : wrapeMethods) {
+            boolean foundMethod = !wrapeeMethod.getDeclaringClass().getPackage().getName().contains(".internal.");
+            if (!foundMethod) {
+                for (Method wraperMethod : wraperMethods) {
+                    foundMethod |= compareMethods(wrapeeMethod, wraperMethod);
+                }
             }
-         }
 
-         if (!foundMethod) missingMethods.add(getMethodSignature(wrapeeMethod));
-      }
-      String link = " (" + wrapingClass.getSimpleName() + ".java:" + 1 + ")";
-      assertTrue("Missing methods in " + link + ". Proposal: \n" + toString(missingMethods, "", "\n\n", "", Object::toString), missingMethods
-            .isEmpty());
-   }
+            if (!foundMethod) missingMethods.add(getMethodSignature(wrapeeMethod));
+        }
+        String link = " (" + wrapingClass.getSimpleName() + ".java:" + 1 + ")";
+        assertTrue("Missing methods in " + link + ". Proposal: \n" + toString(missingMethods, "", "\n\n", "", Object::toString), missingMethods
+                .isEmpty());
+    }
 
-   private boolean compareMethods(Method wrapeeMethod, Method wraperMethod) {
-      Class<?>[] wrapeeParameterTypes = wrapeeMethod.getParameterTypes();
-      if (wrapeeMethod.getName().equals(wraperMethod.getName())) {
-         wrapeeMethod.getReturnType().getName().equals(wraperMethod.getReturnType().getName());
-         Class<?>[] wraperParameterTypes = wraperMethod.getParameterTypes();
-         if (wrapeeParameterTypes.length == wraperParameterTypes.length) {
-            if (wrapeeParameterTypes.length > 0) {
-               for (Class<?> p1 : wrapeeParameterTypes) {
-                  for (Class<?> p2 : wraperParameterTypes) {
-                     if (p1.getName().equals(p2.getName())) { return true; }
-                  }
-               }
-            } else {
-               return true;
+    private boolean compareMethods(Method wrapeeMethod, Method wraperMethod) {
+        Class<?>[] wrapeeParameterTypes = wrapeeMethod.getParameterTypes();
+        if (wrapeeMethod.getName().equals(wraperMethod.getName())) {
+            wrapeeMethod.getReturnType().getName().equals(wraperMethod.getReturnType().getName());
+            Class<?>[] wraperParameterTypes = wraperMethod.getParameterTypes();
+            if (wrapeeParameterTypes.length == wraperParameterTypes.length) {
+                if (wrapeeParameterTypes.length > 0) {
+                    for (Class<?> p1 : wrapeeParameterTypes) {
+                        for (Class<?> p2 : wraperParameterTypes) {
+                            if (p1.getName().equals(p2.getName())) {
+                                return true;
+                            }
+                        }
+                    }
+                } else {
+                    return true;
+                }
             }
-         }
-      }
-      return wrapeeMethod.isAnnotationPresent(Deprecated.class);
-   }
+        }
+        return wrapeeMethod.isAnnotationPresent(Deprecated.class);
+    }
 
-   protected String getMethodSignature(Method m) {
-      return "public " + m.getReturnType().getSimpleName() + " " + generateSignature(m) + " {\n   return " + m.getDeclaringClass().getName() + "." +
-             generateCall(m) + ";\n}";
-   }
+    protected String getMethodSignature(Method m) {
+        return "public " + m.getReturnType().getSimpleName() + " " + generateSignature(m) + " {\n   return " + m.getDeclaringClass().getName() + "." +
+               generateCall(m) + ";\n}";
+    }
 
-   private String generateSignature(Method m) {
-      return m.getName() + toString(m.getParameters(), "(", ", ", ")", param -> param.getType().getSimpleName() + " " + param.getName());
-   }
+    private String generateSignature(Method m) {
+        return m.getName() + toString(m.getParameters(), "(", ", ", ")", param -> param.getType().getSimpleName() + " " + param.getName());
+    }
 
-   private String generateCall(Method m) {
-      return m.getName() + toString(m.getParameters(), "(", ", ", ")", Parameter::getName);
-   }
+    private String generateCall(Method m) {
+        return m.getName() + toString(m.getParameters(), "(", ", ", ")", Parameter::getName);
+    }
 
-   private static <O> String toString(Iterable<O> it, String prefix, String separator, String postfix, Function<O, String> fun) {
-      StringBuffer buff = new StringBuffer(prefix);
-      boolean isFirst = true;
-      for (O o : it) {
-         if (!isFirst) buff.append(separator);
-         buff.append(fun.apply(o));
-         isFirst = false;
-      }
-      buff.append(postfix);
-      return buff.toString();
-   }
+    private static <O> String toString(Iterable<O> it, String prefix, String separator, String postfix, Function<O, String> fun) {
+        StringBuffer buff = new StringBuffer(prefix);
+        boolean isFirst = true;
+        for (O o : it) {
+            if (!isFirst) buff.append(separator);
+            buff.append(fun.apply(o));
+            isFirst = false;
+        }
+        buff.append(postfix);
+        return buff.toString();
+    }
 
-   public static <O> String toString(O[] it, String prefix, String separator, String postfix, Function<O, String> fun) {
-      return toString(Arrays.asList(it), prefix, separator, postfix, fun);
-   }
+    public static <O> String toString(O[] it, String prefix, String separator, String postfix, Function<O, String> fun) {
+        return toString(Arrays.asList(it), prefix, separator, postfix, fun);
+    }
 
 }
