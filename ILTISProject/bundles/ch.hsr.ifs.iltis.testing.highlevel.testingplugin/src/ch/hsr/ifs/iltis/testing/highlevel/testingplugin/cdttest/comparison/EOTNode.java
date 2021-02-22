e717bae5d6c1b1b2f025a12bfc2466c3b6724aff
Felix Morgner <felix.morgner@gmail.com>
2021-02-22 13:27:31 +0100

    iltis: merge testing highlevel

---
+++ ILTISProject/bundles/ch.hsr.ifs.iltis.testing.highlevel.testingplugin/src/ch/hsr/ifs/iltis/testing/highlevel/testingplugin/cdttest/comparison/EOTNode.java
@@ -0,0 +1,139 @@
+package ch.hsr.ifs.iltis.testing.highlevel.testingplugin.cdttest.comparison;
+
+import org.eclipse.cdt.core.dom.ast.ASTNodeProperty;
+import org.eclipse.cdt.core.dom.ast.ASTVisitor;
+import org.eclipse.cdt.core.dom.ast.ExpansionOverlapsBoundaryException;
+import org.eclipse.cdt.core.dom.ast.IASTFileLocation;
+import org.eclipse.cdt.core.dom.ast.IASTNode;
+import org.eclipse.cdt.core.dom.ast.IASTNodeLocation;
+import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.parser.IToken;
+
+import ch.hsr.ifs.iltis.core.exception.ILTISException;
+
+
+/**
+ * This is a marker node to mark the end of an AST
+ *
+ * @author tstauber
+ *
+ */
+public class EOTNode implements IASTNode {
+
+    private IASTTranslationUnit ast;
+
+    protected EOTNode(IASTTranslationUnit ast) {
+        this.ast = ast;
+    }
+
+    private ILTISException makeMarkerException() {
+        return new ILTISException("This node type does not provide any functionality. It is a pure marker node!");
+
+    }
+
+    @Override
+    public IASTTranslationUnit getTranslationUnit() {
+        return this.ast;
+    }
+
+    @Override
+    public IASTNodeLocation[] getNodeLocations() {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public IASTFileLocation getFileLocation() {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public String getContainingFilename() {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public boolean isPartOfTranslationUnitFile() {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public IASTNode getParent() {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public IASTNode[] getChildren() {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public void setParent(IASTNode node) {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public ASTNodeProperty getPropertyInParent() {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public void setPropertyInParent(ASTNodeProperty property) {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public boolean accept(ASTVisitor visitor) {
+        return false;
+    }
+
+    @Override
+    public String getRawSignature() {
+        return "-- END OF TREE --";
+    }
+
+    @Override
+    public boolean contains(IASTNode node) {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public IToken getLeadingSyntax() throws ExpansionOverlapsBoundaryException, UnsupportedOperationException {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public IToken getTrailingSyntax() throws ExpansionOverlapsBoundaryException, UnsupportedOperationException {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public IToken getSyntax() throws ExpansionOverlapsBoundaryException {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public boolean isFrozen() {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public boolean isActive() {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public IASTNode copy() {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public IASTNode copy(CopyStyle style) {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+    @Override
+    public IASTNode getOriginalNode() {
+        throw makeMarkerException().rethrowUnchecked();
+    }
+
+}
---
+++ ILTISProject/bundles/ch.hsr.ifs.iltis.testing.highlevel.testingplugin/src/ch/hsr/ifs/iltis/testing/highlevel/testingplugin/helpers/FileCache.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Institute for Software, HSR Hochschule fuer Technik
+ * Rapperswil, University of applied sciences and others
+ * All rights reserved.
+ *
+ * Contributors:
+ * Institute for Software - initial API and implementation
+ ******************************************************************************/
+package ch.hsr.ifs.iltis.testing.highlevel.testingplugin.helpers;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.ui.editors.text.TextFileDocumentProvider;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+
+import ch.hsr.ifs.iltis.core.resources.FileUtil;
+
+
+public class FileCache {
+
+    private Map<URI, IFile>               mappedURIs         = new HashMap<>();
+    private Map<IFile, IDocumentProvider> connectedDocuments = new HashMap<>();
+    private IDocumentProvider             provider           = new TextFileDocumentProvider();
+
+    /**
+     * Do not call this method before before performing any changes
+     **/
+    public IDocument getDocument(IFile file) {
+        if (connectedDocuments.containsKey(file)) {
+            /* load from cache */
+            return connectedDocuments.get(file).getDocument(file);
+        }
+        try {
+            if (file == null) {
+                return null;
+            }
+            file.getProject();
+            provider.connect(file);
+            connectedDocuments.put(file, provider);
+            return provider.getDocument(file);
+        } catch (CoreException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Do not call this method before before performing any changes
+     **/
+    public IDocument getDocument(URI fileUri) {
+        if (mappedURIs.containsKey(fileUri)) return getDocument(mappedURIs.get(fileUri));
+        IFile file = FileUtil.toIFile(fileUri);
+
+        mappedURIs.put(fileUri, file);
+        return getDocument(file);
+    }
+
+    public void clean() {
+        for (Entry<IFile, IDocumentProvider> curEntry : connectedDocuments.entrySet()) {
+            curEntry.getValue().disconnect(curEntry.getKey());
+        }
+        mappedURIs.clear();
+        connectedDocuments.clear();
+    }
+
+}
